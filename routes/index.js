5a095aaa0d1a547ca42a4ac9959af13ccf764d99
Ero Osamuyi <eroosamuyi11451@gmail.com>
2019-07-22 05:05:16 -0700

    this is my update

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
-});
-router.get('/index1', function(req, res, next) {
-  res.render('pages/index1', { title: 'Express' });
-});
-router.get('/index', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+  res.render('pages/home', { title: 'Express' });
 });
 router.get('/home', function(req, res, next) {
   res.render('pages/home', { title: 'Express' });
 });
 router.get('/about', function(req, res, next) {
   res.render('pages/about', { title: 'Express' });
-});
+}); 
 router.get('/contact', function(req, res, next) {
   res.render('pages/contact', { title: 'Express' });
 });
